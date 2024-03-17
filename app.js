8465b9984975d39dfd47ebecfc0507291d45439e
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-03-17 18:10:23 +0500

    Add JavaScript

---
+++ app.js
@@ -0,0 +1,5 @@
+document.write ("<h1>Favourite Cars</h1>");
+
+var carName= ["Audi", "BMW", "Alfa Romeo", "Limbarghani", "Toyota"]
+
+document.write(`1. ${carName[0]} <br> 2. ${carName[1]} <br>3. ${carName[2]} <br>4. ${carName[3]} <br>5. ${carName[4]} <br>`)
